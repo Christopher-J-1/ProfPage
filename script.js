2161c38c46b396c07f607e99c48c8ed808aaac86
Christopher Jones <chrisjtheonly@gmail.com>
2021-04-29 13:56:36 -0500

    making connections

---
+++ script.js
@@ -0,0 +1,9 @@
+function disappear(){
+    document.getElementById('cookies').style.display = "none"; 
+}
+function disappear2(){
+    document.getElementById('cookies2').style.display = "none"; 
+}
+function changename(){
+    document.getElementById('jane').innerHTML = "any other name";
+}
